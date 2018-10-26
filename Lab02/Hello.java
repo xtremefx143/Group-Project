8e5b2d296df18b29b38afa44b0461059b3a8d03c
giobarrientos <giobarrientos@gios-mbp.lan>
2018-10-25 21:01:47 -0700

    cleaned up

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,12 +6,9 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219380682  - This line was added by Andrew Adkins.");
-<<<<<<< HEAD
 		System.out.println("217805862 - This line was added by Gio Barrientos.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-=======
 		System.out.println("217724131 - This line was added by Marina Stangl.");
->>>>>>> branch 'master' of https://github.com/xtremefx143/Group-Project.git
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
