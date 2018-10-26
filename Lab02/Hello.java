eff3eb8b12247fce9a1e594885922523283d5931
giobarrientos <giobarrientos@gios-mbp.lan>
2018-10-25 21:07:23 -0700

    re-ordered

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,9 +6,8 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219380682  - This line was added by Andrew Adkins.");
-		System.out.println("217805862 - This line was added by Gio Barrientos.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("217724131 - This line was added by Marina Stangl.");
+		System.out.println("217805862 - This line was added by Gio Barrientos.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
