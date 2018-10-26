f3be5372de72be7e9e70677a9ffe6a475e5c6bf3
giobarrientos <giobarrientos@gios-mbp.lan>
2018-10-25 20:28:50 -0700

    commit 1- gio

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219380682  - This line was added by Andrew Adkins.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217805862 - This line was added by Gio Barrientos.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
